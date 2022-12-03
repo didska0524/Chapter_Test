c3d03fc6d70c2decf0553e4fe899704a7c7822ad
DESKTOP-KT09742\seoil <didska0524@gmail.com>
2022-12-03 12:01:04 +0900

    Route fix

--- src/Ch05/Bonus1/ShapeApp.java
+++ src/Ch05/Bonus1/ShapeApp.java
@@ -1,12 +1,12 @@
-package Bonus1;
+package Ch05.Bonus1;
 //201804077최환규
 //보너스1번문제
 
 interface Shape{
-    final double PI = 3.14;         // 상수
+    double PI = 3.14;         // 상수
     void draw();                    // 추상 메소드
     double getArea();               // 추상 메소드
-    default public void redraw(){   // 코드가 작성된 디폴트 메소드
+    default void redraw(){   // 코드가 작성된 디폴트 메소드
         System.out.println("--- 다시 그립니다. ---");
         draw();
     }
